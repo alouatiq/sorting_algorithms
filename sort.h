6e15e41b330e910c4c94f0f09df9167c7081ec72
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-26 11:00:53 +0200

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,26 @@
+### 2. **sort.h**
+```c
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* Struct for doubly linked list */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes for sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+/* Helper function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
