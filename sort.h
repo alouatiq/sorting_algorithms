cbd3cf4fd95eca2c82f2a391c8423606bf444c18
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-26 11:39:01 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -6,9 +6,9 @@
 /* Struct for doubly linked list */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /* Function prototypes for sorting algorithms */
