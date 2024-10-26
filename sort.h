2fb67192f6b42331f04fee394c2cb9c2f74a3a5a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-26 13:05:31 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -6,16 +6,49 @@
 /* Struct for doubly linked list */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
+
+/* Struct for deck of cards */
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
 
 /* Function prototypes for sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+/* Advanced sorting algorithms */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 /* Helper function prototypes */
 void print_array(const int *array, size_t size);
