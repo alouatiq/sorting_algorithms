987754bbd94eaf41a120dc10725186a4fa646808
AL OUATIQ <alouatiq@outlook.com>
2024-10-26 03:19:56 -0700

    fixing betty

--- main/3-main.c
+++ main/3-main.c
@@ -3,20 +3,20 @@
 #include "sort.h"
 
 /**
- * main - Entry point for testing Quick Sort
- *
- * Return: Always 0
- */
+	* main - Entry point for testing Quick Sort
+	*
+	* Return: Always 0
+	*/
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
 
-    printf("Original array:\n");
-    print_array(array, n);
-    printf("\nSorting using Quick Sort:\n");
-    quick_sort(array, n);
-    printf("\nSorted array:\n");
-    print_array(array, n);
-    return (0);
+	printf("Original array:\n");
+	print_array(array, n);
+	printf("\nSorting using Quick Sort:\n");
+	quick_sort(array, n);
+	printf("\nSorted array:\n");
+	print_array(array, n);
+	return (0);
 }
