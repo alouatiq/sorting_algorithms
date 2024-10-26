ec7a8ac8433d9f04db35b8786dde0fed9161b969
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-26 12:15:27 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -29,9 +29,9 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 	if (low < high)
 	{
 	int pi = lomuto_partition(array, low, high, size);
-	
+
 	quick_sort_recursive(array, low, pi - 1, size);
-	
+
 	quick_sort_recursive(array, pi + 1, high, size);
 	}
 }
